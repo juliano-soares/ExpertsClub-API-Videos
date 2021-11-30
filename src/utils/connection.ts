3205eeeed7e4b3acc5f1e70eeffdc54ded495aa2
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2021-11-29 23:57:14 -0300

    Correção no mongoose.connect.

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const dotenv = require('dotenv')
 dotenv.config()
 
-mongoose.connect(process.env.CONNECTION_MOONGOSE, 
+mongoose.connect(`${process.env.CONNECTION_MOONGOSE}`, 
     {
         //useNewUrlParser: true,
         //useUnifiedTopology: true
